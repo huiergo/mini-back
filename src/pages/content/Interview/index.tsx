2b5ee5ebf8685d85245a6a195b24576f05148d63
huiergo <928187057@qq.com>
2022-04-28 11:39:45 +0800

    feat: interview ui

--- src/pages/content/Interview/index.tsx
+++ src/pages/content/Interview/index.tsx
@@ -1,7 +1,5 @@
 import { Button, Result } from 'antd';
 import React from 'react';
-import { history } from 'umi';
-import { instance } from '@/auth';
 
 const NoFoundPage: React.FC = () => <Result status="404" title="敬请期待~" />;
 
